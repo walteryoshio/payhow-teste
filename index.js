5f52ee341f6f64719de0018d8c01e273ecb20607
Walter Kudo <walter.kudo@lionsoft.com.br>
2020-12-16 13:45:04 -0300

    teste

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-function teste(value){
-
-    const mult = value * value
-
-    return (mult)
-
+function formatCep(value){
+    value = String(value)
+    .replace(/\D/g, "")
+    .slice(0,8)
+    .replace(/\d{5}{\d}/, "$1-$2")
 }
+console.log(formatCep(564156))
 module.exports = {
-    teste
+    formatCep
 }
